375fccbab01004d71ae109940eaf94b21c2f0f41
Arulraj Aboorvasamy <95745238+ArulrajAboo@users.noreply.github.com>
2024-02-09 11:11:35 +0530

    Committed the example project.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Getting-Started-with-the-Vue-Diagram-Component
-A quick-start Vue project that shows how to add a Syncfusion Vue Diagram component to a Vue project. It includes code snippets to create nodes and connectors, join the nodes using connectors, and add segments and annotations to the connectors.
+# Getting Started with the Vue Diagram Component
+
+A quick-start Vue project that shows how to add a Syncfusion [Vue Diagram]( https://www.syncfusion.com/vue-components/vue-diagram?utm_source=github&utm_medium=listing&utm_campaign=vue-diagram-gettingstarted-sample) component to a Vue project. It includes code snippets to create nodes and connectors, join the nodes using connectors, and add segments and annotations to the connectors.
+
+Watch the video: Coming soon...
+
+Refer to the following documentation to learn about the Vue Diagram component: https://ej2.syncfusion.com/vue/documentation/diagram/getting-started-vue-3
+
+Check out this online example of the Vue Diagram component: https://ej2.syncfusion.com/vue/demos/#/material3/diagram/default-functionality.html
+
+Make sure you have the latest versions of Node.js and Visual Studio Code on your machine before working with this project.
+
+## How to run this application
+To run this application, you need to clone the `Getting-Started-with-the-Vue-Diagram-Component` repository and then open it in Visual Studio Code. After that, just install all the necessary Vue packages in your project using the `npm install` command and run your project using the `npm run dev` command.
---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <ejs-diagram id="diagram" :width="width" :height="height" :nodes="nodes" :getNodeDefaults="getNodeDefaults"
+    :connectors="connectors"></ejs-diagram>
+</template>
+<script>
+import { DiagramComponent } from '@syncfusion/ej2-vue-diagrams';
+
+let nodes = [
+  {
+    id: "node1",
+    //height: 60,
+    //width: 170,
+    //offsetX: 600,
+    offsetY: 50,
+    shape: { type: "Flow", shape: "Terminator" },
+    annotations: [{ content: "Start" }]
+  },
+  {
+    id: "node2",
+    offsetY: 140,
+    shape: { type: "Flow", shape: "Process" },
+    annotations: [{ content: "create variable i = 0" }]
+  },
+  {
+    id: "node3",
+    offsetY: 230,
+    shape: { type: "Flow", shape: "Decision" },
+    annotations: [{ content: "is i < 10?" }]
+  },
+  {
+    id: "node4",
+    offsetY: 340,
+    shape: { type: "Flow", shape: "PreDefinedProcess" },
+    annotations: [
+      {
+        content: 'print "Syncfusion!!"'
+      }
+    ]
+  },
+  {
+    id: "node5",
+    offsetY: 430,
+    shape: { type: "Flow", shape: "Process" },
+    annotations: [
+      {
+        content: "i = i + 1"
+      }
+    ]
+  },
+  {
+    id: "node6",
+    offsetY: 520,
+    shape: { type: "Flow", shape: "Terminator" },
+    annotations: [{ content: "End" }]
+  }
+];
+
+let connectors = [
+  {
+    id: "connector1",
+    sourceID: "node1",
+    targetID: "node2"
+  },
+  {
+    id: "connector2",
+    sourceID: "node2",
+    targetID: "node3"
+  },
+  {
+    id: "connector3",
+    sourceID: "node3",
+    targetID: "node4",
+    annotations: [{ content: "Yes", style: { fill: "white"}}]
+  },
+  {
+    id: "connector4",
+    sourceID: "node4",
+    targetID: "node5"
+  },
+  {
+    id: "connector5",
+    sourceID: "node3",
+    targetID: "node6",
+    type: "Orthogonal",
+    segments:[
+      { length: 30, direction: "Right"},
+      { length: 300, direction: "Bottom"}
+    ],
+    annotations: [{ content: "No", style: { fill: "white" } }]
+  },
+  {
+    id: "connector6",
+    sourceID: "node5",
+    targetID: "node3",
+    type: "Orthogonal",
+        segments: [
+            { length: 30, direction: "Left" },
+            { length: 200, direction: "Top" }
+        ]
+  }
+];
+
+export default {
+  components: {
+    "ejs-diagram": DiagramComponent
+  },
+
+  data: function () {
+    return {
+      width: "1250px",
+      height: "560px",
+      nodes: nodes,
+      connectors: connectors,
+      getNodeDefaults: (node) => {
+        node.height = 60;
+        node.width = 170;
+        node.offsetX = 600;
+        return node;
+      }
+    }
+  }
+}
+</script>
+<style>
+@import "../node_modules/@syncfusion/ej2-vue-diagrams/styles/material.css";
+</style>
